2f7ff4a2a2c0763aca04469281da4382a393d363
Zeeshan@Expocon <developer@expocon.app>
2023-12-04 19:02:14 +0530

    other episode

--- Index.js
+++ Index.js
@@ -1,6 +1,10 @@
-const heading = React.createElement("div", {id:"parent"} , [ React.createElement("h1", {id : "child"}), [React.createElement("h1",{}, "My second heading"),React.createElement("h2",{}, "My second heading") ],
-[React.createElement("h1", {id : "child"}), [React.createElement("h1",{}, "My second heading"),React.createElement("h2",{}, "My second heading") ]]]  );
+import React from "react"
+import ReactDOM from "react-dom/client"
 
+const heading = React.createElement("div", {id:"parent"} , [ React.createElement("h1", {id : "child"}), [React.createElement("h1",{}, "My second heading 🚀"),React.createElement("h2",{}, "My second heading") ],
+[React.createElement("h1", {id : "child"}), React.createElement("h1",{}, "My second heading"),React.createElement("h2",{}, "My second heading") ] 
+]  );
+
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-console.log(heading);
